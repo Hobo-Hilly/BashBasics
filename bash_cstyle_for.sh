73f075605be5c64237fb6a86b51f051c0b55383b
Hilly@localhost <vincentcid96@gmail.com>
2022-04-05 01:40:12 +0000

    More Bash notes.This is part two of the three part series from itprotv

--- bash_cstyle_for.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((a = 1; a <= 11; a++))
-do
-  echo "Counting is fun $a"
-done
-
--- bash_for.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-var1=$(cat /etc//passwd | awk '{FS = ":"} {print $1}')
-	for i in $var1
-	do
-		echo "The next username is ...>" $i
-	done
-
