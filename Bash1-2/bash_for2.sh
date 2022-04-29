e99d36b19915e968a67440e57dd3caa511de9631
Hilly@localhost <vincentcid96@gmail.com>
2022-04-29 03:39:37 +0000

    Just working through concepts in bash

--- Bash1-2/bash_for2.sh
+++ Bash1-2/bash_for2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Pirates_Movies=$(cat /home/Hilly/BASH/Bash1-2/Pirates_Of_The_Caribbean_Movies | cut -d ":" -f "1")
+Pirates_Movies=$(cat /home/Hilly/BASH/Bash1-2/Pirates_Of_The_Caribbean_Movies | awk '{FS = "/"} {print $1}'  )
 	for i in $Pirates_Movies
 	do
 		echo $Pirates_Movies "is one of the movies in the Pirates of the The Caribbean series"
